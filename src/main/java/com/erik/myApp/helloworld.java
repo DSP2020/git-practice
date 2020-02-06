4df56800de2290da08f8d47d797e1ce1529240c7
erikbustosmo@gmail.com <[erikbustosmo@gmail.com]>
2020-02-06 09:01:15 -0600

    initial commit and added new maven dependencies in POM

---
+++ src/main/java/com/erik/myApp/helloworld.java
@@ -0,0 +1,10 @@
+package com.erik.myApp;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
